fda93e818d90044ab4785ffa0dadd07fc1bd06ad
Hack Reactor Students <accounts@hackreactor.com>
2016-10-13 11:24:30 -0700

    added grunt concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-    },
+        options: {
+          separator: ';',
+        },
+        dist: {
+          src: ['public/client/*.js']
+          dest: 'public/dist/concat.js',
+        },
+      },
 
     mochaTest: {
       test: {
@@ -78,7 +85,7 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
+  grunt.registerTask('build', [ 'concat'
   ]);
 
   grunt.registerTask('upload', function(n) {
